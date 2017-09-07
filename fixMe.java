da05575a7bc6e7061057a93cea9a93000a3bb412
woodchuck33 <jezaki33@gmail.com>
2017-09-06 21:37:20 -0700

    Update and rename FixMe.java to fixMe.java

--- fixMe.java
+++ fixMe.java
@@ -1,15 +1,15 @@
-    public class FixMe
+    public class fixMe
 {public static void main (String[] args){
                   System.out.println("There's something wrong with this");
   System.out.println("Can you fix me?");
-        nextMethod();
+        NextMethod();
                     }
-                        public static void nextMethod()
+                        public static void NextMethod()
 {
         System.out.println("Are you annoyed yet?");
-oneMore();
+OneMore();
                                                         }
-                                                        public static void oneMore()
+                                                        public static void OneMore()
 {
                     System.out.println("You can see how this could be frustrating, right?");
 }
