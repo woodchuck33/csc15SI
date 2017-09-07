fbb90f419426145a691e0dd066682106666a4c2d
Ryan Broman <ryanjbroman@gmail.com>
2017-09-07 09:34:30 -0700

    Update fixMe.java

--- fixMe.java
+++ fixMe.java
@@ -1,16 +1,14 @@
-    public class fixMe
-{public static void main (String[] args){
-                  System.out.println("There's something wrong with this");
-  System.out.println("Can you fix me?");
+public class fixMe {
+    public static void main (String[] args) {
+        System.out.println("There's something wrong with this");
+        System.out.println("Can you fix me?");
         NextMethod();
-                    }
-                        public static void NextMethod()
-{
+    }
+    public static void NextMethod() {
         System.out.println("Are you annoyed yet?");
-OneMore();
-                                                        }
-                                                        public static void OneMore()
-{
-                    System.out.println("You can see how this could be frustrating, right?");
+        OneMore();
+    }
+    public static void OneMore() {
+        System.out.println("You can see how this could be frustrating, right?");
+    }
 }
-                                      }
