151516de5ca2e077afe90da6eff0957fdba80f52
woodchuck33 <jezaki33@gmail.com>
2017-09-06 21:25:04 -0700

    Update Chapter1Program.java

--- Chapter1Program.java
+++ Chapter1Program.java
@@ -1,8 +1,11 @@
 public class Chapter1
 {
-  public static void main(String[] args)
-  {
-  }
-  
-  
+public static void main(String[] args)
+{
+writeSomething()
+}
+public static void writeSomething()
+{
+System.out.println("Hello, World");
+}
 }
