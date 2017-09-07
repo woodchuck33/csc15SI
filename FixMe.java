1240b03cf5f745d46096b7a57a5307dbc124505b
woodchuck33 <jezaki33@gmail.com>
2017-09-06 21:30:24 -0700

    Create FixMe.java

---
+++ FixMe.java
@@ -0,0 +1,16 @@
+    public class FixMe
+{public static void main (String[] args){
+                  System.out.println("There's something wrong with this");
+  System.out.println("Can you fix me?");
+        nextMethod();
+                    }
+                        public static void nextMethod()
+{
+        System.out.println("Are you annoyed yet?");
+oneMore();
+                                                        }
+                                                        public static void oneMore()
+{
+                    System.out.println("You can see how this could be frustrating, right?");
+}
+                                      }
