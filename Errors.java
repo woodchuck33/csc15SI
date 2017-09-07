67482037b7a6e0ca884da9f065d440c0edfd2879
woodchuck33 <jezaki33@gmail.com>
2017-09-06 21:45:56 -0700

    Create Errors.java

---
+++ Errors.java
@@ -0,0 +1,12 @@
+public class Errors
+{
+    public static void (String[] args)
+    {
+        System.out.println("Hello, World);
+        MoreErrors;
+    }
+    
+    public static void moreErrors()
+    {
+        System.out.prntln("Here's some more errors")
+}
